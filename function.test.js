37255dd27f6527e36c5a470b7e1bff12dd312471
Gayathri Buddhika <39160904+gayathribuddhika@users.noreply.github.com>
2021-11-28 21:02:44 +0530

    add another test case

--- function.test.js
+++ function.test.js
@@ -2,4 +2,12 @@ const functions = require('./function');
 
 test('Add two numbers 2 and 2 equal to 4', () => {
     expect(functions.add(2,2)).toBe(4);
+});
+
+test('Add two numbers 2 and 2 not equal to 5', () => {
+    expect(functions.add(2,2)).not.toBe(5);
+});
+
+test('Should be Null', () => {
+    expect(functions.isNull()).toBeNull();
 })

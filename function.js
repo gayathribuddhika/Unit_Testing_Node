e2dcd78dbc18c0155cea3beb79a0a487bc3dffa4
Gayathri Buddhika <39160904+gayathribuddhika@users.noreply.github.com>
2021-11-29 22:14:10 +0530

    add new test methods and functions

--- function.js
+++ function.js
@@ -1,6 +1,12 @@
 const functions = {
     add: (num1, num2) => num1+ num2,
-    isNull: () => null
+    isNull: () => null,
+    checkvalue: x => x,
+    createUser: () => {
+        const user = { firstname: "gayathri" }
+        user["lastname"] = "buddhika";
+        return user;
+    }
 };
 
 module.exports = functions;
--- function.test.js
+++ function.test.js
@@ -10,4 +10,17 @@ test('Add two numbers 2 and 2 not equal to 5', () => {
 
 test('Should be Null', () => {
     expect(functions.isNull()).toBeNull();
-})
+});
+
+test('Should be falsy', () => {
+    expect(functions.checkvalue(0)).toBeFalsy(); // test pass null, 0, undefined words
+});
+
+test('User should be gayathri buddhika', () => {
+    expect(functions.createUser()).toEqual({
+        firstname: "gayathri",
+        lastname: "buddhika"
+    });
+});
+
+
